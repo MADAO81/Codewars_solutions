87a4be87eb9529d76edc86a37fcf036231116943
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-03-07 21:32:03 +0300

    Update and rename Mumbling 7 kyu.py to Mumbling_7_kyu.py

--- 7 kyu/Mumbling_7_kyu.py
+++ 7 kyu/Mumbling_7_kyu.py
@@ -7,12 +7,14 @@
 # accum("cwAt") -> "C-Ww-Aaa-Tttt"
 # The parameter of accum is a string which includes only letters from a..z and A..Z.
 
-def accum(st):
-    return "-".join([st[i].upper()+(st[i].lower()* i) for i in range(len(st))])
-    
+
 
 # def accum(s):
 #     output = ""
 #     for i in range(len(s)):
 #         output+=(s[i]*(i+1))+"-"
 #     return output.title()[:-1]
+
+def accum(st):
+    return "-".join([st[i].upper()+(st[i].lower()* i) for i in range(len(st))])
+    
