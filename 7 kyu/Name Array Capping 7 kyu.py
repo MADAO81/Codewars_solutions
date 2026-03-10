8650d22fb90ac9b10afeea3bffd97a258bea1584
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-03-10 09:36:44 +0300

    Update Name Array Capping 7 kyu.py

--- 7 kyu/Name Array Capping 7 kyu.py
+++ 7 kyu/Name Array Capping 7 kyu.py
@@ -1,3 +1,4 @@
+# https://www.codewars.com/kata/5356ad2cbb858025d800111d
 # Name Array Capping
 # Create a function that accepts an array of names, and returns 
 # an array of each name with its first letter capitalized and the remainder in lowercase.
@@ -6,13 +7,22 @@
 # ['jo', 'nelson', 'jurie'] -->  ['Jo', 'Nelson', 'Jurie']
 # ['KARLY', 'DANIEL', 'KELSEY'] --> ['Karly', 'Daniel', 'Kelsey']
 
+
+# def cap_me(arr):
+#     result = []
+#     for ch in arr:
+#         if ch == "":
+#             result.append(ch)
+#         else:
+#             result.append(ch[0].upper()+ch[1:].lower())
+#    return result
+
+
+
+# def cap_me(arr):
+#     return list(map(str.title, arr))
+
+
+
 def cap_me(arr):
-    result = []
-    for ch in arr:
-        if ch == "":
-            result.append(ch)
-        else:
-            result.append(ch[0].upper()+ch[1:].lower())
-    return result
-    
-# return [name.capitalize() for name in arr]
+    return [name.capitalize() for name in arr]
