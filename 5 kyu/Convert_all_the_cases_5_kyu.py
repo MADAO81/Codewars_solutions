a48888e044bd10be2cfed0a03bcee47b0330fbec
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-03-11 09:27:13 +0300

    Update and rename Convert all the cases 5 kyu.py to Convert_all_the_cases_5_kyu.py

--- 5 kyu/Convert_all_the_cases_5_kyu.py
+++ 5 kyu/Convert_all_the_cases_5_kyu.py
@@ -20,20 +20,7 @@
 
 # (Any translations would be greatly appreciated!)
 
-import re
 
-def change_case(label, target):
-    if ('_' in label) + ('-' in label) + (label != label.lower()) > 1:
-        return
-    
-    if target == 'snake':
-        return re.sub('([A-Z])', r'_\1', label.replace('-', '_')).lower()
-    
-    if target == 'kebab':
-        return re.sub('([A-Z])', r'-\1', label.replace('_', '-')).lower()
-    
-    if target == 'camel':
-        return re.sub('([_-])([a-z])', lambda m: m.group(2).upper(), label)
 
 # import re
 # def change_case(identifier, target_case):
@@ -66,3 +53,21 @@ def change_case(label, target):
 #         return "".join(mo)
 #     else:
 #         return None
+
+
+
+import re
+
+def change_case(label, target):
+    if ('_' in label) + ('-' in label) + (label != label.lower()) > 1:
+        return
+    
+    if target == 'snake':
+        return re.sub('([A-Z])', r'_\1', label.replace('-', '_')).lower()
+    
+    if target == 'kebab':
+        return re.sub('([A-Z])', r'-\1', label.replace('_', '-')).lower()
+    
+    if target == 'camel':
+        return re.sub('([_-])([a-z])', lambda m: m.group(2).upper(), label)
+
