bbe9c96f30c21e939507208ea747a6495372e401
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-03-18 09:11:38 +0300

    Update and rename Predict your age 7 kyu.py to Predict_your_age_7_kyu.py

--- 7 kyu/Predict_your_age_7_kyu.py
+++ 7 kyu/Predict_your_age_7_kyu.py
@@ -11,8 +11,6 @@
 # Example
 # predict_age(65, 60, 75, 55, 60, 63, 64, 45) == 86
 
-def predict_age(age_1, age_2, age_3, age_4, age_5, age_6, age_7, age_8):
-    return ((age_1**2+age_2**2+ age_3**2+ age_4**2+ age_5**2+age_6**2+ age_7**2+age_8**2)**0.5)//2
 
 
 # import numpy as np
@@ -22,3 +20,7 @@ def predict_age(age_1, age_2, age_3, age_4, age_5, age_6, age_7, age_8):
 
 # def predict_age(*ages):
 #     return sum(a*a for a in ages)**.5//2
+
+
+def predict_age(age_1, age_2, age_3, age_4, age_5, age_6, age_7, age_8):
+    return ((age_1**2+age_2**2+ age_3**2+ age_4**2+ age_5**2+age_6**2+ age_7**2+age_8**2)**0.5)//2
