ca392ef28548e9ffe79264f238c2b6a43e9da6aa
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-03-19 08:57:54 +0300

    Update Snail 4 kyu.py

--- 4 kyu/Snail 4 kyu.py
+++ 4 kyu/Snail 4 kyu.py
@@ -12,39 +12,43 @@
 #          [7,6,5]]
 # snail(array) #=> [1,2,3,4,5,6,7,8,9]
 
-def snail(snail_map):
-    results = []
-    while len(snail_map) > 0:
-        # right
-        results += snail_map[0] 
-        del snail_map[0]
+
+# Solution_1
+# def snail(snail_map):
+#     results = []
+#     while len(snail_map) > 0:
+#         # right
+#         results += snail_map[0] 
+#         del snail_map[0]
         
-        if len(snail_map) > 0:
-            # down
-            for i in snail_map:
-                results += [i[-1]]
-                del i[-1]
+#         if len(snail_map) > 0:
+#             # down
+#             for i in snail_map:
+#                 results += [i[-1]]
+#                 del i[-1]
                 
-            # left
-            if snail_map[-1]:
-                results +=snail_map[-1][::-1]
-                del snail_map[-1]
+#             # left
+#             if snail_map[-1]:
+#                 results +=snail_map[-1][::-1]
+#                 del snail_map[-1]
                 
-            # top
-            for i in reversed(snail_map):
-                results +=[i[0]]
-                del i[0]
+#             # top
+#             for i in reversed(snail_map):
+#                 results +=[i[0]]
+#                 del i[0]
                 
-    return results
+#     return results
 
 
 
-# import numpy as np
+# Solution_2
 
-# def snail(array):
-#     m = []
-#     array = np.array(array)
-#     while len(array) > 0:
-#         m += array[0].tolist()
-#         array = np.rot90(array[1:])
-#     return m
+import numpy as np
+
+def snail(array):
+    m = []
+    array = np.array(array)
+    while len(array) > 0:
+        m += array[0].tolist()
+        array = np.rot90(array[1:])
+    return m
