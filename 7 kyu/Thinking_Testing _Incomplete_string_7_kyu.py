6e39a33c5dd52fc38768de99799e13c0ad1ec49b
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-03-25 09:23:27 +0300

    Update and rename Thinking Testing  Incomplete string 7 kyu.py to Thinking_Testing _Incomplete_string_7_kyu.py

--- 7 kyu/Thinking_Testing _Incomplete_string_7_kyu.py
+++ 7 kyu/Thinking_Testing _Incomplete_string_7_kyu.py
@@ -7,12 +7,15 @@
 
 # Look at result of testcase, guess the code!
 
-def testit(s):
-    arr = [chr((ord(s[i]) + ord(s[i+1]))//2) for i in range(0,len(s)-1,2)]
-    return "".join(arr) if len(s) % 2 == 0 else "".join(arr) + s[-1]
-    
+
 
 # import re
 
 # def testit(s):
 #     return re.sub("(.)(.)", lambda m: chr((ord(m[1]) + ord(m[2])) // 2), s)
+
+
+def testit(s):
+    arr = [chr((ord(s[i]) + ord(s[i+1]))//2) for i in range(0,len(s)-1,2)]
+    return "".join(arr) if len(s) % 2 == 0 else "".join(arr) + s[-1]
+    
