e634228d7cf36e58e3fbe67f8fdb35111925a4d7
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-03-29 18:22:09 +0300

    Update and rename Good VS Evil 6kyu.py to Good_VS_Evil_6_kyu.py

--- 6 kyu/Good_VS_Evil_6_kyu.py
+++ 6 kyu/Good_VS_Evil_6_kyu.py
@@ -1,16 +1,6 @@
 # https://www.codewars.com/kata/52761ee4cffbc69732000738/train/python
 
-def good_vs_evil(good, evil):
-    good = map(int,good.split(" "))
-    evil = map(int,evil.split(" "))
-    good_total = sum(map( lambda x: x[0]*x[1], zip(good, [1,2,3,3,4,10])))
-    evil_total = sum(map( lambda x: x[0]*x[1], zip(evil, [1,2,2,2,3,5,10])))
-    if good_total > evil_total:
-        return "Battle Result: Good triumphs over Evil"
-    elif good_total < evil_total:
-        return "Battle Result: Evil eradicates all trace of Good"
-    else:
-        return "Battle Result: No victor on this battle field"
+
 
 
 # def goodVsEvil(good, evil):
@@ -29,3 +19,16 @@ def good_vs_evil(good, evil):
 #         return result + 'Good triumphs over Evil'
 #     else:
 #         return result + 'No victor on this battle field'
+
+
+def good_vs_evil(good, evil):
+    good = map(int,good.split(" "))
+    evil = map(int,evil.split(" "))
+    good_total = sum(map( lambda x: x[0]*x[1], zip(good, [1,2,3,3,4,10])))
+    evil_total = sum(map( lambda x: x[0]*x[1], zip(evil, [1,2,2,2,3,5,10])))
+    if good_total > evil_total:
+        return "Battle Result: Good triumphs over Evil"
+    elif good_total < evil_total:
+        return "Battle Result: Evil eradicates all trace of Good"
+    else:
+        return "Battle Result: No victor on this battle field"
