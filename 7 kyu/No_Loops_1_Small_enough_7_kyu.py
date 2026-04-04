742489f78dfe2955d2958455ac4488884e33f342
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-04-04 20:52:04 +0300

    Create No_Loops_1_Small_enough_7_kyu.py

---
+++ 7 kyu/No_Loops_1_Small_enough_7_kyu.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/57cc4853fa9fc57a6a0002c2/train/python
+
+
+# def small_enough(a, limit): 
+#     return all(map(lambda x: x <= limit, a))
+
+
+
+def small_enough(a, limit): 
+    return max(a) <= limit
