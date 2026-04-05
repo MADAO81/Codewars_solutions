75418928ee1e586baf90036c934eed842cb562df
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-04-05 18:16:42 +0300

    Update and rename Greed is Good 5 kyu.py to Greed_is_Good_5_kyu.py

--- 5 kyu/Greed_is_Good_5_kyu.py
+++ 5 kyu/Greed_is_Good_5_kyu.py
@@ -21,19 +21,6 @@
 
 
 
-from collections import Counter
-def score(dice):
-    score = {1:1000, 6:600, 5:500, 4:400, 3:300, 2:200}
-    dices = Counter(dice)
-    total = 0
-    for i,j in dices.items():
-        if j >=3:
-            total += score[i] * (j//3)
-        if i ==1:
-            total += 100 * (j%3)
-        elif i == 5:
-            total +=50*(j%3)
-    return total
 
 
 # def score(dice): 
@@ -48,3 +35,18 @@ def score(dice):
 #     sum += (points[i] if count >= 3 else 0) + extra[i] * (count%3)
 
 #   return sum 
+
+from collections import Counter
+def score(dice):
+    score = {1:1000, 6:600, 5:500, 4:400, 3:300, 2:200}
+    dices = Counter(dice)
+    total = 0
+    for i,j in dices.items():
+        if j >=3:
+            total += score[i] * (j//3)
+        if i ==1:
+            total += 100 * (j%3)
+        elif i == 5:
+            total +=50*(j%3)
+    return total
+
