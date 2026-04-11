62582fda8316e3e1bbf6c7f3a08eb8c790232bb0
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-04-11 20:37:49 +0300

    Update Mirror_mirror_on_the_wall_7_kyu.py

--- 7 kyu/Mirror_mirror_on_the_wall_7_kyu.py
+++ 7 kyu/Mirror_mirror_on_the_wall_7_kyu.py
@@ -1,6 +1,16 @@
 # https://www.codewars.com/kata/5f55ecd770692e001484af7d/train/python
 
+# def mirror(data: list) -> list:
+#     x = sorted(data)
+#     y = x[::-1][1:]
+#     return x+y
+
+
+# def mirror(data):
+#     return (s := sorted(data)) + s[-2::-1]
+
+
+
 def mirror(data: list) -> list:
-    x = sorted(data)
-    y = x[::-1][1:]
-    return x+y
+    arr = sorted(data)
+    return arr + arr[-2::-1]
