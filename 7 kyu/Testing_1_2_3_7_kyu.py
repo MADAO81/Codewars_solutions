51b1f18ae05b418a519882bcf9c8144f797bf516
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-04-14 09:39:47 +0300

    Update and rename Testing 1-2-3 7 kyu.py to Testing_1_2_3_7_kyu.py

--- 7 kyu/Testing_1_2_3_7_kyu.py
+++ 7 kyu/Testing_1_2_3_7_kyu.py
@@ -10,16 +10,18 @@
 # [] --> []
 # ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
 
-def number(lines):
-    counter = 1
-    number_line = 0
-    result = []
-    for i in lines:
-        number_line = str(counter) + ": " + i
-        result.append(number_line)
-        counter +=1
-    return result
 
 
 # def number(lines):
-# 	return [str(x+1) + ": " + lines[x] for x in range(len(lines))]
+#     counter = 1
+#     number_line = 0
+#     result = []
+#     for i in lines:
+#         number_line = str(counter) + ": " + i
+#         result.append(number_line)
+#         counter +=1
+#     return result
+
+
+def number(lines):
+	return [str(x+1) + ": " + lines[x] for x in range(len(lines))]
