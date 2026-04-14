b64918d5207b46115db02ffb2b370b26b318f561
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-04-14 09:39:12 +0300

    Update and rename Sum of Cubes 7 kyu.py to Sum_of_Cubes_7_kyu.py

--- 7 kyu/Sum_of_Cubes_7_kyu.py
+++ 7 kyu/Sum_of_Cubes_7_kyu.py
@@ -7,11 +7,13 @@
 # 2 --> 9 (sum of the cubes of 1 and 2 is 1 + 8)
 # 3 --> 36 (sum of the cubes of 1, 2, and 3 is 1 + 8 + 27)
 
+
+# def sum_cubes(n):
+#     return sum(i**3 for i in range(0,n+1))
+
+
 def sum_cubes(n):
     result = 0
     for i in range(1,n+1):
         result += i**3
     return result
-
-# def sum_cubes(n):
-#     return sum(i**3 for i in range(0,n+1))
