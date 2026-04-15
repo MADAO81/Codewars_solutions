eee825d2d9dc77850225820c4c32c03c1c2f343a
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-04-15 09:09:46 +0300

    Update and rename Remove duplicate words 7 kyu.py to Remove_duplicate_words_7_kyu.py

--- 7 kyu/Remove_duplicate_words_7_kyu.py
+++ 7 kyu/Remove_duplicate_words_7_kyu.py
@@ -10,12 +10,16 @@
 
 # 'alpha beta gamma delta'
 
-def remove_duplicate_words(s):
-    result = []
-    for word in s.split():
-        if word not in result:
-            result.append(word)
-    return " ".join(result)
+
 
 # def remove_duplicate_words(s):
-#     return ' '.join(dict.fromkeys(s.split()))
+#     result = []
+#     for word in s.split():
+#         if word not in result:
+#             result.append(word)
+#     return " ".join(result)
+
+
+
+def remove_duplicate_words(s):
+    return ' '.join(dict.fromkeys(s.split()))
