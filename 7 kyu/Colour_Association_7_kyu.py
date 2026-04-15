6324050f10045798eb3c4216eb673ff031cedafd
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-04-15 09:09:06 +0300

    Create  Colour_Association_7_kyu.py

---
+++ 7 kyu/Colour_Association_7_kyu.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/56d6b7e43e8186c228000637/train/python
+
+# def colour_association(arr):
+#     return [{item[0]: item[1]} for item in arr]
+
+
+def colour_association(arr):
+    return [{k: v} for k, v in arr]
