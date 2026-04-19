c7c2149a2f6be641caf8ec91677a63b3dc4ffb10
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-04-19 18:21:34 +0300

    Update and rename Simple equation reversal 7 kyu.py to Simple_equation_reversal_7_kyu.py

--- 7 kyu/Simple_equation_reversal_7_kyu.py
+++ 7 kyu/Simple_equation_reversal_7_kyu.py
@@ -3,13 +3,16 @@
 # solve("100*b/y") = "y/b*100"
 # solve("a+b-c/d*30") = "30*d/c-b+a"
 
-import re
 
-def solve(eq):
-    return ''.join(reversed(re.split(r'(\W+)', eq)))
 
 
 # import re
 
 # def solve(eq):
 #     return "".join(re.split("([*+-/])", eq)[::-1])
+
+
+import re
+
+def solve(eq):
+    return ''.join(reversed(re.split(r'(\W+)', eq)))
