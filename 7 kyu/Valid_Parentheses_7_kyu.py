3550c1b2d751bc890097e16e42f51534351bc5d1
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-04-27 09:19:09 +0300

    Update and rename Valid Parentheses 7 kyu.py to Valid_Parentheses_7_kyu.py

--- 7 kyu/Valid_Parentheses_7_kyu.py
+++ 7 kyu/Valid_Parentheses_7_kyu.py
@@ -14,19 +14,22 @@
 
 # Protip: If you are trying to figure out why a string of parentheses is invalid, paste the parentheses into the code editor, and let the code highlighting show you!
 
-def valid_parentheses(paren_str):
-    flag = 0
-    for parenth in paren_str:
-        if parenth == "(":
-            flag +=1
-        elif parenth == ")":
-            flag -=1
-        if flag < 0:
-            return False
-    return True if flag == 0 else False
 
 
-# def valid_parentheses(x):
-#     while "()" in x:
-#         x = x.replace("()", "")
-#     return x == ""
+# def valid_parentheses(paren_str):
+#     flag = 0
+#     for parenth in paren_str:
+#         if parenth == "(":
+#             flag +=1
+#         elif parenth == ")":
+#             flag -=1
+#         if flag < 0:
+#             return False
+#     return True if flag == 0 else False
+
+
+
+def valid_parentheses(x):
+    while "()" in x:
+        x = x.replace("()", "")
+    return x == ""
