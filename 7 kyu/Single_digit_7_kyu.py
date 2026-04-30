f465c95a79197656344cd70d928919bb2b058a81
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-04-30 09:43:02 +0300

    Create Single_digit_7_kyu.py

---
+++ 7 kyu/Single_digit_7_kyu.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/5a7778790136a132a00000c1/train/python
+
+
+# def single_digit(n):
+#   return n if n<10 else single_digit(bin(n).count("1"))
+
+
+def single_digit(n):
+    while n>9:
+        n = bin(n).count('1')
+    return n
