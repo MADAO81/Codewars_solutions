90c6bf988995ed87feb974ef17be27232f8f47b8
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-05-06 09:11:29 +0300

    Create Sum_Square_Even_Root_Odd_7_kyu.py

---
+++ 7 kyu/Sum_Square_Even_Root_Odd_7_kyu.py
@@ -0,0 +1,16 @@
+# https://www.codewars.com/kata/5a4b16435f08299c7000274f/train/python
+
+# from math import sqrt
+
+# def sum_square_even_root_odd(nums):
+#     sum_nums = 0
+#     for i in nums:
+#         if i%2 == 0:
+#             sum_nums += pow(i,2)
+#         else:
+#             sum_nums += sqrt(i)
+            
+#     return round(sum_nums, 2)
+
+
+
