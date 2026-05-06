be086d1c4ad7fc5b4c9a1859fefd5fa9d9efc738
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-05-06 09:14:38 +0300

    Update and rename The highest profit wins 7 kyu.py to The_highest_profit_wins_7_kyu.py

--- 7 kyu/The_highest_profit_wins_7_kyu.py
+++ 7 kyu/The_highest_profit_wins_7_kyu.py
@@ -15,11 +15,15 @@
 # All arrays or lists will always have at least one element, so you don't need to check the length.
 # Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
 
-def min_max(lst):
-    result = []
-    result.append(int(min(lst)))
-    result.append(int(max(lst)))
-    return result
-    
+
+
 # def min_max(lst):
-#   return [min(lst), max(lst)]
+#     result = []
+#     result.append(int(min(lst)))
+#     result.append(int(max(lst)))
+#     return result
+
+
+
+def min_max(lst):
+  return [min(lst), max(lst)]
