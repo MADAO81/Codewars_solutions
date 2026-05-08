a3d8d0b99da7aebd3068a1281d2c9858a5d8f8b9
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-05-08 09:27:56 +0300

    Update and rename Be Concise IV - Index of an element in an array 8 kyu.py to Be_Concise_IV_Index_of_an_element_in_an_array_8_kyu.py

--- 8 kyu/Be_Concise_IV_Index_of_an_element_in_an_array_8_kyu.py
+++ 8 kyu/Be_Concise_IV_Index_of_an_element_in_an_array_8_kyu.py
@@ -3,10 +3,13 @@
 # our task is to shorten the code as much as possible in order to meet the strict character count requirements of the Kata. 
 # (no more than 161) You may assume that all array elements are unique.
 
-def find(a, e):
-        return a.index(e) if e in a else 'Not found'
+
 
 
 # def find(arr, e):
 #     try: return arr.index(e)
 #     except: return "Not found"
+
+
+def find(a, e):
+        return a.index(e) if e in a else 'Not found'
