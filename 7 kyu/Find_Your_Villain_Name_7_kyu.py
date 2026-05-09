6531ef4610ff717f7a2c036d599a66792fe1b0b7
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-05-09 18:16:36 +0300

    Create Find_Your_Villain_Name_7_kyu.py

---
+++ 7 kyu/Find_Your_Villain_Name_7_kyu.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/536c00e21da4dc0a0700128b/train/python
+
+
+def get_villain_name(birthdate): 
+    first = [ "The Evil","The Vile","The Cruel", "The Trashy","The Despicable", "The Embarrassing", "The Disreputable","The Atrocious", "The Twirling",  "The Orange","The Terrifying", "The Awkward"]
+    last = ["Mustache", "Pickle", "Hood Ornament", "Raisin", "Recycling Bin", "Potato", "Tomato", "House Cat", "Teaspoon", "Laundry Basket"]
+    return first[birthdate.month - 1] + ' ' + last[birthdate.day % 10]
