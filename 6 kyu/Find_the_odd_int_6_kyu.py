9a6235935757975b32e4656043d00ab7a0a5a773
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-05-12 09:06:21 +0300

    Update and rename Find the odd int 6 kyu.py to Find_the_odd_int_6_kyu.py

--- 6 kyu/Find_the_odd_int_6_kyu.py
+++ 6 kyu/Find_the_odd_int_6_kyu.py
@@ -10,13 +10,16 @@
 # [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
 # [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
 
-def find_it(seq):
-    for i in seq:
-        if seq.count(i) % 2 != 0:
-            return i
+
 
 # def find_it(seq):
 #     result = 0
 #     for x in seq:
 #         result ^= x
 #     return result
+
+
+def find_it(seq):
+    for i in seq:
+        if seq.count(i) % 2 != 0:
+            return i
