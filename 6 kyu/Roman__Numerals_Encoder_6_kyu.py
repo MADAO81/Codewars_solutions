2803b191eea2a88ccbaa353c07ea7fb40b198a11
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-05-14 09:44:23 +0300

    Update and rename Roman Numerals Encoder 6 kyu.py to Roman__Numerals_Encoder_6_kyu.py

--- 6 kyu/Roman__Numerals_Encoder_6_kyu.py
+++ 6 kyu/Roman__Numerals_Encoder_6_kyu.py
@@ -25,6 +25,19 @@
 
 
 
+
+
+# def solution(n):
+#     dic = {1:'I',4:'IV',5:'V',9:'IX',10:'X',40:'XL',50:'L',90:'XC',100:'C',400:'CD',500:'D',900:'CM',1000:'M'}
+#     roman = ''
+#     for a in reversed(sorted(dic.keys())):
+#         while (a <= n):
+#             n = n - a;
+#             roman = roman + dic[a];
+#     return roman
+
+
+
 def solution(n):
     roman_numerals = {1000:'M',
                       900: 'CM',
@@ -46,12 +59,3 @@ def solution(n):
             roman_string += roman_numerals[key]
             n -= key
     return roman_string
-
-# def solution(n):
-#     dic = {1:'I',4:'IV',5:'V',9:'IX',10:'X',40:'XL',50:'L',90:'XC',100:'C',400:'CD',500:'D',900:'CM',1000:'M'}
-#     roman = ''
-#     for a in reversed(sorted(dic.keys())):
-#         while (a <= n):
-#             n = n - a;
-#             roman = roman + dic[a];
-#     return roman
