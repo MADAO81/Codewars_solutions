fc9eb3512a65c3632e9b2b2bacf05f0d20c7b286
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-05-16 20:54:56 +0300

    Create Elapsed_Seconds_7_kyu.py

---
+++ 7 kyu/Elapsed_Seconds_7_kyu.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/517b25a48557c200b800000c/train/python
+
+
+# def elapsed_seconds(start, end):
+#     return (end-start).seconds
+
+def elapsed_seconds(start, end):
+    return (end-start).total_seconds()
