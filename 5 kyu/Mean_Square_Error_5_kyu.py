4713720d78a06d899c1fc5d42a8ec01e5b007245
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-05-25 08:58:07 +0300

    Update and rename Mean Square Error 5 kyu.py to Mean_Square_Error_5_kyu.py

--- 5 kyu/Mean_Square_Error_5_kyu.py
+++ 5 kyu/Mean_Square_Error_5_kyu.py
@@ -4,6 +4,15 @@
 # -compares the value each member in one array to the corresponding member in the other
 # -squares the absolute value difference between those two values
 # -and returns the average of those squared absolute value difference between each member pair.
+
+
+# def solution(a, b):
+#     return sum((x - y)**2 for x, y in zip(a, b)) / len(a)
+
+# def solution(a, b):
+#     return sum((a[i]-b[i])**2 for i in range(len(a)))/len(a)
+
+
 def solution(array_a, array_b):
     sum_of_squares = 0
     result = 0
@@ -12,9 +21,3 @@ def solution(array_a, array_b):
         sum_of_squares += (array_a[i]-array_b[i])**2
     result = sum_of_squares / len(array_a)
     return result
-
-# def solution(a, b):
-#     return sum((x - y)**2 for x, y in zip(a, b)) / len(a)
-
-# def solution(a, b):
-#     return sum((a[i]-b[i])**2 for i in range(len(a)))/len(a)
