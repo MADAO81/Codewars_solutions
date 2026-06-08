c9b162868718c38663bb93ecfb58e10948359c91
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-06-08 08:58:56 +0300

    Create Twice_as_old_8_kyu.py

---
+++ 8 kyu/Twice_as_old_8_kyu.py
@@ -0,0 +1,12 @@
+# https://www.codewars.com/kata/5b853229cfde412a470000d0/train/python
+
+# def twice_as_old(dad_years_old, son_years_old):
+#     result = dad_years_old - 2 * son_years_old
+#     if result >0:
+#         return result   
+#     else:
+#         return result *(-1)
+
+
+def twice_as_old(dad_years_old, son_years_old):
+    return abs(dad_years_old - 2*son_years_old)
