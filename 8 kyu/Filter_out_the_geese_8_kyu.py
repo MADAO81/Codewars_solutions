491561e74a6c6eefb68d47b3adaf2199e2d7910d
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-06-11 09:08:08 +0300

    Update and rename Filter out the geese 8 kyu.py to Filter_out_the_geese_8_kyu.py

--- 8 kyu/Filter_out_the_geese_8_kyu.py
+++ 8 kyu/Filter_out_the_geese_8_kyu.py
@@ -12,16 +12,19 @@
 # The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. 
 # Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
 
-geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
-def goose_filter(birds):
-    without_geese = []
-    for bird in birds:
-        if bird not in geese:
-            without_geese.append(bird)
-    return without_geese
 
 
-# geese = {"African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"}
-
+# geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
 # def goose_filter(birds):
-#     return [bird for bird in birds if bird not in geese]
+#     without_geese = []
+#     for bird in birds:
+#         if bird not in geese:
+#             without_geese.append(bird)
+#     return without_geese
+
+
+
+geese = {"African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"}
+
+def goose_filter(birds):
+    return [bird for bird in birds if bird not in geese]
