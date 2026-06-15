a2e4603c4fbcb1fca6b2628c7bcccf04d2a8f1fb
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-06-15 08:59:21 +0300

    Update and rename Find numbers which are divisible by given number 8 kyu.py to Find_numbers_which_are_divisible_by_given_number_8_kyu.py

--- 8 kyu/Find_numbers_which_are_divisible_by_given_number_8_kyu.py
+++ 8 kyu/Find_numbers_which_are_divisible_by_given_number_8_kyu.py
@@ -5,8 +5,6 @@
 # Example(Input1, Input2 --> Output)
 # [1, 2, 3, 4, 5, 6], 2 --> [2, 4, 6]
 
-def divisible_by(numbers, divisor):
-    return [x for x in numbers if x%divisor==0]
 
 # def divisible_by(numbers, divisor):
 #     result = []
@@ -14,3 +12,7 @@ def divisible_by(numbers, divisor):
 #         if number % divisor == 0:
 #             result.append(number)
 #     return result
+
+
+def divisible_by(numbers, divisor):
+    return [x for x in numbers if x%divisor==0]
