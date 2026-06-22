f882a20d0865a57da882071f6662b8e584c91272
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-06-22 09:07:17 +0300

    Update and rename Find the first non-consecutive number 8 kyu.py to Find_the_first_non-consecutive_number_8_kyu.py

--- 8 kyu/Find_the_first_non-consecutive_number_8_kyu.py
+++ 8 kyu/Find_the_first_non-consecutive_number_8_kyu.py
@@ -13,18 +13,20 @@
 
 # If you like this Kata, maybe try this one next: https://www.codewars.com/kata/represent-array-of-numbers-as-ranges
 
-def first_non_consecutive(arr):
-    i = 1
-    for x in arr:
-        if i < len(arr) and arr[i] - arr[i-1] != 1:
-            return arr[i]
 
-        i += 1
-    return None
-            
             
 # def first_non_consecutive(arr):
 #     if not arr: return 0
 #     for i, x in enumerate(arr[:-1]):
 #         if x + 1 != arr[i + 1]:
 #             return arr[i + 1]
+
+
+def first_non_consecutive(arr):
+    i = 1
+    for x in arr:
+        if i < len(arr) and arr[i] - arr[i-1] != 1:
+            return arr[i]
+        i += 1
+    return None
+            
