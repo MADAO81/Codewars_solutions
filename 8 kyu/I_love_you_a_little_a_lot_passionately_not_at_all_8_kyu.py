546b69ff7e8f8b82f1b307495e44618d5396e2f3
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-07-05 18:48:18 +0300

    Update and rename I love you, a little , a lot, passionately  not at all 8 kyu.py to I_love_you_a_little_a_lot_passionately_not_at_all_8_kyu.py

--- 8 kyu/I_love_you_a_little_a_lot_passionately_not_at_all_8_kyu.py
+++ 8 kyu/I_love_you_a_little_a_lot_passionately_not_at_all_8_kyu.py
@@ -12,13 +12,15 @@
 
 # Your goal in this kata is to determine which phrase the girls would say at the last petal for a flower of a given number of petals. The number of petals is always greater than 0.
 
-def how_much_i_love_you(nb_petals):
-    phrase = ["not at all", "I love you","a little","a lot","passionately","madly"]
-    return phrase[nb_petals % 6]
-
 
 # def how_much_i_love_you(nb_petals):
 #     # your code
 #     phrases = {1: 'I love you', 2: 'a little', 3: 'a lot', 4: 'passionately',
 #             5: 'madly', 0: 'not at all'}
 #     return phrases[nb_petals%6]
+
+
+def how_much_i_love_you(nb_petals):
+    phrase = ["not at all", "I love you","a little","a lot","passionately","madly"]
+    return phrase[nb_petals % 6]
+
