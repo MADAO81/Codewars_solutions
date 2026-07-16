a1269e5a6d2ff797b2a84e3acf573d6df1e486c7
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-07-16 09:47:30 +0300

    Update Complete_Series_7_kyu.py

--- 7 kyu/Complete_Series_7_kyu.py
+++ 7 kyu/Complete_Series_7_kyu.py
@@ -1,5 +1,9 @@
 # https://www.codewars.com/kata/580a4001d6df740d61000301/train/python
 
 
+# def complete_series(a):
+#     return list(range(max(a) + 1)) if len(a) == len(set(a)) else [0]
+    
+
 def complete_series(seq): 
     return [0] if len(seq) != len(set(seq)) else list(range(max(seq) + 1))
