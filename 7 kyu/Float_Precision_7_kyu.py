7b14444ef6f64041a97c28a518bf3516132b2002
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-07-18 19:04:48 +0300

    Create  Float_Precision_7_kyu.py

---
+++ 7 kyu/Float_Precision_7_kyu.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/5143d157ceb46d6a61000001/train/python
+
+# def solution(n):
+#     return float('{:0.2f}'.format(n))
+  
+
+def solution(x: float) -> float:
+    return round(x,2)
