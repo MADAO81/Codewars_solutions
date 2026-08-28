c83d1e390779805b10a70c55aa6edcbbb5541319
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-08-28 09:27:17 +0300

    Update and rename Online RPG player to qualifying stage 8 kyu.py to Online_RPG_player_to_qualifying_stage_8_kyu.py

--- 8 kyu/Online_RPG_player_to_qualifying_stage_8_kyu.py
+++ 8 kyu/Online_RPG_player_to_qualifying_stage_8_kyu.py
@@ -12,13 +12,16 @@
 # NOTE
 # : Remember, in C# you have to cast your output value to Object type!
 
-def playerRankUp(pts):
-    if pts >= 100:
-        return "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up."
-    else:
-        return False
+
 
 
 # def playerRankUp(pts):
 #     msg = "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up." 
 #     return msg if pts >=100 else False
+
+
+def playerRankUp(pts):
+    if pts >= 100:
+        return "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up."
+    else:
+        return False
