f0f175e166e934ad45e19236299ab1a8c15e8143
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-08-30 11:18:23 +0300

    Update and rename Pillars 8 kyu.py to Pillars_8_kyu.py

--- 8 kyu/Pillars_8_kyu.py
+++ 8 kyu/Pillars_8_kyu.py
@@ -5,11 +5,14 @@
 # width of the pillar (10 - 50 centimeters).
 # Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
 
-def pillars(num_pill, dist, width):
-    if num_pill == 1:
-        return 0
-    return (num_pill - 1) * (dist * 100) + (width * num_pill) - (2 * width)
+
 
 
 # def pillars(num_pill, dist, width):
 #     return dist * 100 * (num_pill - 1) + width * (num_pill - 2) * (num_pill > 1)
+
+
+def pillars(num_pill, dist, width):
+    if num_pill == 1:
+        return 0
+    return (num_pill - 1) * (dist * 100) + (width * num_pill) - (2 * width)
