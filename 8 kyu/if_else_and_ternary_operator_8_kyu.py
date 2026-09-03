193e9924b39629ed3fb17734fbf416c888255472
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-09-03 08:53:32 +0300

    Update and rename if else and ternary operator 8 kyu.py to if_else_and_ternary_operator_8_kyu.py

--- 8 kyu/if_else_and_ternary_operator_8_kyu.py
+++ 8 kyu/if_else_and_ternary_operator_8_kyu.py
@@ -14,13 +14,15 @@
 
 # In the end, click "Submit" to submit your code and pass this kata.
 
-def sale_hotdogs(n):
-    if n < 5:
-        return n * 100
-    elif 5 <= n <10:
-        return n * 95
-    else:
-        return n * 90
-        
+
 # def sale_hotdogs(n):
-#     return n * (100 if n < 5 else 95 if n < 10 else 90)
+#     if n < 5:
+#         return n * 100
+#     elif 5 <= n <10:
+#         return n * 95
+#     else:
+#         return n * 90
+        
+        
+def sale_hotdogs(n):
+    return n * (100 if n < 5 else 95 if n < 10 else 90)
