68d4068022f62efc247b8d9aa556c6443b345d23
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-09-12 20:21:14 +0300

    Update and rename Well of Ideas - Easy Version 8 kyu.py to Well_of_Ideas_Easy_Version_8_kyu.py

--- 8 kyu/Well_of_Ideas_Easy_Version_8_kyu.py
+++ 8 kyu/Well_of_Ideas_Easy_Version_8_kyu.py
@@ -4,18 +4,18 @@
 # If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. 
 # If there are no good ideas, as is often the case, return 'Fail!'.
 
-def well(x):
-    count = 0
-    for ch in x:
-        if ch == "good":
-            count +=1
-    if count == 0:
-        return "Fail!"
-    elif 1<=count<=2:
-        return "Publish!"
-    else:
-        return "I smell a series!"
+# def well(x):
+#     count = 0
+#     for ch in x:
+#         if ch == "good":
+#             count +=1
+#     if count == 0:
+#         return "Fail!"
+#     elif 1<=count<=2:
+#         return "Publish!"
+#     else:
+#         return "I smell a series!"
 
-  # def well(x):
-  #   c = x.count('good')
-  #   return 'I smell a series!' if c > 2 else 'Publish!' if c else 'Fail!'
+  def well(x):
+    c = x.count('good')
+    return 'I smell a series!' if c > 2 else 'Publish!' if c else 'Fail!'
