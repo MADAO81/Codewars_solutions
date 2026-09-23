745567ec9739f39fadca722cca717dcce85ea170
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2026-09-23 08:52:46 +0300

    Update and rename Thinkful - Logic Drills Traffic light 8 kyu.py to Thinkful_Logic_Drills_Traffic_light_8_kyu.py

--- 8 kyu/Thinkful_Logic_Drills_Traffic_light_8_kyu.py
+++ 8 kyu/Thinkful_Logic_Drills_Traffic_light_8_kyu.py
@@ -5,16 +5,16 @@
 # the current state of the light and returns a string representing the state the light should change to.
 # For example, when the input is green, output should be yellow.
 
-def update_light(current):
-    if current == "green":
-        return "yellow"
-    elif current == "yellow":
-        return "red"
-    elif current == "red":
-        return "green"
-    else:
-        return "there is no such color on the traffic light"
+# def update_light(current):
+#     if current == "green":
+#         return "yellow"
+#     elif current == "yellow":
+#         return "red"
+#     elif current == "red":
+#         return "green"
+#     else:
+#         return "there is no such color on the traffic light"
         
 
-# def update_light(current):
-#     return {"green": "yellow", "yellow": "red", "red": "green"}[current]
+def update_light(current):
+    return {"green": "yellow", "yellow": "red", "red": "green"}[current]
